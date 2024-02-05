49487d22ffcb0b505a5da05c42e918aa1590d241
Aubrey McCutchan <aubreymccutchan@yahoo.com>
2024-02-05 21:25:54 +0100

    Added access to additional plant tables.

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import random
 app = Flask(__name__, static_url_path='/static')
 
 db_config = get_secret()
-# print((db_config))
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     f'postgresql+psycopg2://{db_config["POSTGRES_USER"]}:' +
@@ -22,8 +21,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-class Plants(db.Model):
-    __tablename__ = 'trees'
+
+class BasePlant(db.Model):
+    __abstract__ = True
 
     plant_id = db.Column(db.Integer, primary_key=True)
     plant_name = db.Column(db.String())
@@ -39,6 +39,13 @@ class Plants(db.Model):
         self.scientific_name = scientific_name
         self.plant_type = plant_type
 
+class Trees(BasePlant):
+    __tablename__ = 'trees'
+
+class Flowers(BasePlant):
+    __tablename__ = 'flower'
+        
+
 @app.route('/')
 def render_webpage():
     plants = []
@@ -47,7 +54,7 @@ def render_webpage():
     # Retrieve 4 unique plants
     while len(plants) < 4:
         # Execute the query to retrieve a random plant
-        random_plant = Plants.query.order_by(db.func.random()).first()
+        random_plant = Trees.query.order_by(db.func.random()).first()
 
         # Check if the plant name is unique
         if random_plant.plant_name not in unique_plant_names:
@@ -67,25 +74,32 @@ def render_webpage():
 
 @app.route('/get_plant_name_list')
 def get_plant_name_list():
-
     plants = []
     unique_plant_names = set()
 
-    # Retrieve 4 unique plants
+    # Retrieve 2 unique plants from each table (total of 4)
     while len(plants) < 4:
-        # Execute the query to retrieve a random plant
-        random_plant = Plants.query.order_by(db.func.random()).first()
+        # Execute the query to retrieve a random plant from 'trees'
+        random_tree = Trees.query.order_by(db.func.random()).first()
 
         # Check if the plant name is unique
-        if random_plant.plant_name not in unique_plant_names:
+        if random_tree.plant_name not in unique_plant_names:
             # Append the unique plant to the list
-            plants.append((random_plant.plant_name, random_plant.image_url, random_plant.scientific_name, random_plant.plant_type))
+            plants.append((random_tree.plant_name, random_tree.image_url, random_tree.scientific_name, random_tree.plant_type))
             
             # Add the plant name to the set of unique names
-            unique_plant_names.add(random_plant.plant_name)
+            unique_plant_names.add(random_tree.plant_name)
 
-    # Now, 'plants' contains 4 unique plants
+        # Execute the query to retrieve a random plant from 'flowers'
+        random_flower = Flowers.query.order_by(db.func.random()).first()
 
+        # Check if the plant name is unique
+        if random_flower.plant_name not in unique_plant_names:
+            # Append the unique plant to the list
+            plants.append((random_flower.plant_name, random_flower.image_url, random_flower.scientific_name, random_flower.plant_type))
+            
+            # Add the plant name to the set of unique names
+            unique_plant_names.add(random_flower.plant_name)
 
     # Generate options for the plant identification
     plant_names = [item[0] for item in plants]
@@ -93,7 +107,7 @@ def get_plant_name_list():
     scientific_names = [item[2] for item in plants]
     plant_types = [item[3] for item in plants]
 
-    return jsonify(plant_names=plant_names, plant_image_url=plant_image_url, scientific_names=scientific_names, plant_types = plant_types)
+    return jsonify(plant_names=plant_names, plant_image_url=plant_image_url, scientific_names=scientific_names, plant_types=plant_types)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000) 
